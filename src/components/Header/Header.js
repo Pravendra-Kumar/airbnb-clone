d32adb514646a0fe0d7716c389411a63c4330b05
Pravendra Kumar <pravendraksj7.com>
2023-03-08 11:56:49 +0530

    Update 8/3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
 import BasicMenu from './ProfileMenu';
 import BottomNav from './BottomNav';
-import MobileSearchBar from '../MobileSearchBar/MobileSearchBar';
+
 
 
 const Header = () => {
@@ -32,7 +32,7 @@ const Header = () => {
         </div>
        
     </div>
-    {/* <MobileSearchBar/> */}
+    
     <BottomNav/>
     </div>
   )
