f3937b73d6291a7f464b5c67cee5fb3dc3953e27
Pravendra Kumar <pravendraksj7.com>
2023-03-08 12:43:46 +0530

    clean json file

--- src/assets/cards-list.js
+++ src/assets/cards-list.js
@@ -57,8 +57,8 @@ export const list = [
       "https://a0.muscache.com/im/pictures/4d08a7d4-342c-4986-b6e8-b24bf2cea961.jpg?im_w=720",
       "https://a0.muscache.com/im/pictures/83df1292-e487-46cb-b953-ec358013f619.jpg?im_w=720",
       "https://a0.muscache.com/im/pictures/c10c4a61-02ec-46f2-819e-bfa2fbbabe19.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/997a8468-9b82-4ead-b7be-59e559354aa0.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/b234fed9-5dd7-43d0-afe0-ea6838221684.jpg?im_w=720"
+      "https://a0.muscache.com/im/pictures/997a8468-9b82-4ead-b7be-59e559354aa0.jpg?im_w=720"
+     
     ],
     price: "20000",
     date: "19-20 Jan",
@@ -93,10 +93,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/prohost-api/Hosting-29862895/original/8ddab15f-ec1a-41d3-9799-b97064211e0d.jpeg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg"      
     ],
     price: "20000",
     date: "15-20 May",
@@ -107,8 +104,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://a0.muscache.com/im/pictures/4d08a7d4-342c-4986-b6e8-b24bf2cea961.jpg?im_w=720",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://a0.muscache.com/im/pictures/4d08a7d4-342c-4986-b6e8-b24bf2cea961.jpg?im_w=720"
     ],
     price: "20000",
     date: "15-20 May",
@@ -119,9 +115,7 @@ export const list = [
     desc: "Amazing pools",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/83df1292-e487-46cb-b953-ec358013f619.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/c10c4a61-02ec-46f2-819e-bfa2fbbabe19.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/997a8468-9b82-4ead-b7be-59e559354aa0.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/b234fed9-5dd7-43d0-afe0-ea6838221684.jpg?im_w=720"
+      "https://a0.muscache.com/im/pictures/c10c4a61-02ec-46f2-819e-bfa2fbbabe19.jpg?im_w=720"
     ],
     price: "20000",
     date: "19-20 Jan",
@@ -132,8 +126,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/miso/Hosting-728889164834947472/original/5e74de0f-9be9-4d97-b2ac-d0221262fdaa.jpeg?im_w=720",
-      "https://a0.muscache.com/im/pictures/miso/Hosting-728889164834947472/original/7d7dd561-2943-4add-8cba-2cdd5bce717c.jpeg?im_w=720",
-      "https://a0.muscache.com/im/pictures/miso/Hosting-728889164834947472/original/9fe484e1-1354-4fc4-8c27-d2b3f5e0a12c.jpeg?im_w=720"
+      "https://a0.muscache.com/im/pictures/miso/Hosting-728889164834947472/original/7d7dd561-2943-4add-8cba-2cdd5bce717c.jpeg?im_w=720"
     ],
     price: "30000",
     date: "25-20 May",
@@ -144,9 +137,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg"
     ],
     price: "20000",
     date: "15-20 May",
@@ -157,10 +148,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/7ef7f41b-62e2-43c1-b68f-8cd23e954167.jpg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg"
     ],
     price: "20000",
     date: "15-20 May",
@@ -171,9 +159,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg"
     ],
     price: "20000",
     date: "15-20 May",
@@ -184,11 +170,8 @@ export const list = [
     desc: "Amazing pools",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/c4977b31-4918-4edb-a6ab-63a9ad74dfd2.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/4d08a7d4-342c-4986-b6e8-b24bf2cea961.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/83df1292-e487-46cb-b953-ec358013f619.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/c10c4a61-02ec-46f2-819e-bfa2fbbabe19.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/997a8468-9b82-4ead-b7be-59e559354aa0.jpg?im_w=720",
-      "https://a0.muscache.com/im/pictures/b234fed9-5dd7-43d0-afe0-ea6838221684.jpg?im_w=720"
+      "https://a0.muscache.com/im/pictures/4d08a7d4-342c-4986-b6e8-b24bf2cea961.jpg?im_w=720"
+     
     ],
     price: "20000",
     date: "19-20 Jan",
@@ -199,10 +182,7 @@ export const list = [
     desc: "Beach and Sunset Views",
     imgSrc: [
       "https://a0.muscache.com/im/pictures/15a3cdb5-b24e-4335-b2ca-bda3b6774dd9.jpg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg"
     ],
     price: "20000",
     date: "15-20 May",
@@ -212,11 +192,7 @@ export const list = [
     rating: "4",
     desc: "Beach and Sunset Views",
     imgSrc: [
-      "https://a0.muscache.com/im/pictures/miso/Hosting-48748370/original/1e882ee5-9c12-4968-99d2-67bf7760a072.jpeg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://a0.muscache.com/im/pictures/miso/Hosting-48748370/original/1e882ee5-9c12-4968-99d2-67bf7760a072.jpeg?im_w=720"
     ],
     price: "20000",
     date: "15-20 May",
@@ -226,11 +202,7 @@ export const list = [
     rating: "4",
     desc: "Beach and Sunset Views",
     imgSrc: [
-      "https://a0.muscache.com/im/pictures/8bd84f5a-b13f-45e0-95e3-6f0601b9f163.jpg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://a0.muscache.com/im/pictures/8bd84f5a-b13f-45e0-95e3-6f0601b9f163.jpg?im_w=720"
     ],
     price: "20000",
     date: "15-20 May",
@@ -240,11 +212,7 @@ export const list = [
     rating: "4",
     desc: "Beach and Sunset Views",
     imgSrc: [
-      "https://a0.muscache.com/im/pictures/b078c905-32b6-43ed-82e9-bcb1108f71a1.jpg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://a0.muscache.com/im/pictures/b078c905-32b6-43ed-82e9-bcb1108f71a1.jpg?im_w=720"
     ],
     price: "20000",
     date: "15-20 May",
@@ -254,11 +222,7 @@ export const list = [
     rating: "4",
     desc: "Beach and Sunset Views",
     imgSrc: [
-      "https://a0.muscache.com/im/pictures/1c360e12-97fe-4bc0-8180-330e270e0483.jpg?im_w=720",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
-      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
-      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://a0.muscache.com/im/pictures/1c360e12-97fe-4bc0-8180-330e270e0483.jpg?im_w=720"
     ],
     price: "20000",
     date: "15-20 May",
--- src/assets/images-links.js
+++ src/assets/images-links.js
@@ -66,54 +66,5 @@ export const links = [
     imgSrc:
       "	https://a0.muscache.com/pictures/c0a24c04-ce1f-490c-833f-987613930eca.jpg",
   }
-  // ,{
-  //   label: "Top of the world",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
-  // }
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
-  // },
-  // {
-  //   label: "Lakefront",
-  //   imgSrc:
-  //     "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
-  // },
+  
 ];
